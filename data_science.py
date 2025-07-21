4f3759be7cb09a409bf6cb85fde8cfc4fda3e299
Juan123654 <jplsslon@hotmail.com>
2025-07-21 10:42:52 -0500

    iniciando analisis

---
+++ data_science.py
@@ -0,0 +1,12 @@
+'''
+Requisitos:
+Analizar datos de la tienda:
+Debes evaluar información como los ingresos, las categorías más vendidas, 
+las reseñas de los clientes, los productos más vendidos y el envío promedio.
+Crear gráficos para visualización:
+Decide qué tipos de gráficos utilizar para presentar los resultados de forma clara y visual.
+Mínimo de 3 gráficos diferentes, que pueden incluir gráficos de barras, circulares, de dispersión y otros.
+Enviar una recomendación:
+Después del análisis, escriba un texto explicando a qué tienda debería vender el 
+Sr. João y por qué, basándose en los datos presentados.
+'''
